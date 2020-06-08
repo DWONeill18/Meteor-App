cd2a4961f5fc7f97475e89018550de99d8e98af1
DWONeill18 <d.w.oneill.18@gmail.com>
2020-06-08 21:46:27 +0100

    Added publication for tasks. Defined method to set tasks to private.

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,22 @@ import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+    // This code only runs on the server
+    // Only publish tasks that are public or belong to the current user
+    Meteor.publish('tasks', function tasksPublication() {
+        return Tasks.find({
+            $or: [{
+                private: {
+                    $ne: true
+                }
+            }, {
+                owner: this.userId
+            }, ],
+        });
+    });
+}
+
 Meteor.methods({
     'tasks.insert' (text) {
         check(text, String);
@@ -35,4 +51,21 @@ Meteor.methods({
             }
         });
     },
-});
+    'tasks.setPrivate' (taskId, setToPrivate) {
+        check(taskId, String);
+        check(setToPrivate, Boolean);
+
+        const task = Task.findOne(taskId);
+
+        // Make sure only the task owner can make a task private
+        if (task.owner !== Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Tasks.update(taskId, {
+            $set: {
+            private: setToPrivate
+            }
+        });
+    },
+    });
